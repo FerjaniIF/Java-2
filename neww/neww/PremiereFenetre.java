7a1a1f01b2fd438bbb38988afeec72b656362c4e
Intissar Ferjani <147095910+FerjaniIF@users.noreply.github.com>
2024-03-12 21:40:32 +0100

    Add files via upload

---
+++ neww/neww/PremiereFenetre.java
@@ -0,0 +1,20 @@
+package neww;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PremiereFenetre extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Group root = new Group();
+		Scene scene = new Scene(root, 300, 250, Color.WHITE);
+		primaryStage.setTitle("Ma première fenêtre");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
